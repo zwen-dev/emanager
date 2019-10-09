6c7545afb6463bb59c271da7cf3d0e3277baf5b1
Zoey Wen <zwen@sbeck-iMac.local>
2019-10-09 12:28:23 -0400

    first commit

---
+++ client/components/App.js
@@ -0,0 +1,18 @@
+import React, {component} from 'react';
+//import logo from './logo.svg';
+import '../css/App.css';
+
+class App extends Component{
+  render(){
+    return(
+        <div className="App">
+            <header className="App-header">
+                <h1 className="App-title">Test example</h1>
+            </header>
+            <p className="App-intro">To get started...</p>
+        </div>
+      );
+  }
+}
+
+export default App;
